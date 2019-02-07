76927af0937ce6dcb5bca1df83df64182b0aa30b
Ibrahim Yousre <ib.yousre@gmail.com>
2019-02-07 18:42:31 +0200

    Initial Commit

---
+++ Client/src/main/java/com/jets/chatproject/client/views/Dummy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jets.chatproject.client.views;
+
+import com.jets.chatproject.client.controllers.*;
+
+/**
+ *
+ * @author ibrahim
+ */
+public interface Dummy {
+    
+}
---
+++ Server/src/main/java/com/jets/chatproject/server/module/rmi/imp/Dummy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jets.chatproject.server.module.rmi.imp;
+
+/**
+ *
+ * @author ibrahim
+ */
+public interface Dummy {
+    
+}
