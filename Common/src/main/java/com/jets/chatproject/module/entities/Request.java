a0eea4aba76267251498f72ed0542f0a4fbdb7d3
salma <salma@DESKTOP-Q5K3J4F>
2019-02-08 16:08:38 +0200

    add request entity

---
+++ Common/src/main/java/com/jets/chatproject/module/entities/Request.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jets.chatproject.module.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author salma
+ */
+public class Request {
+    
+    private int senderId;
+    private int receiverId;
+    private Date requestTime;
+    
+    public Request(int senderId, int receiverId, Date requestTime) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.requestTime = requestTime; 
+   }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(int receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public Date getRequestTime() {
+        return requestTime;
+    }
+
+    public void setRequestTime(Date requestTime) {
+        this.requestTime = requestTime;
+    }
+    
+    
+}
