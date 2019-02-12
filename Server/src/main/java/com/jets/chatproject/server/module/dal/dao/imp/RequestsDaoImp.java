26880f74088c3587dcf7d7cf1a496881be7220cc
Ibrahim Yousre <ib.yousre@gmail.com>
2019-02-12 16:43:28 +0200

    Add RequestsDaoImp

--- Server/src/main/java/com/jets/chatproject/server/module/dal/dao/imp/RequestsDaoImp.java
+++ Server/src/main/java/com/jets/chatproject/server/module/dal/dao/imp/RequestsDaoImp.java
@@ -7,7 +7,15 @@ package com.jets.chatproject.server.module.dal.dao.imp;
 
 import com.jets.chatproject.server.module.dal.dao.RequestsDoa;
 import com.jets.chatproject.server.module.dal.entities.Request;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.DataSource;
 
 /**
@@ -24,22 +32,64 @@ public class RequestsDaoImp implements RequestsDoa {
 
     @Override
     public List<Request> findAllByReceiver(int userId) {
+        List< Request> list = null;
+        try {
+            Connection conn = dataSource.getConnection();
+            String query = "select * from Requests where receiver_id = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            list = new ArrayList<>();
+            while (resultSet.next()) {
+                Request request = new Request(resultSet.getInt(1),
+                        userId, resultSet.getTimestamp(3));
+                list.add(request);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RequestsDaoImp.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    @Override
+    public boolean insert(Request request) {
+        try {
+            Connection conn = dataSource.getConnection();
+            String query = "insert into Requests values(?,?,?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, request.getSenderId());
+            preparedStatement.setInt(2, request.getReceiverId());
+            preparedStatement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(RequestsDaoImp.class.getName())
+                    .log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Request request) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public boolean insert(Request object) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public boolean update(Request object) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public boolean delete(Request object) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean delete(Request request) {
+        try {
+            Connection conn = dataSource.getConnection();
+            String query = "delete from Requests where sender_id = ? and receiver_id = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, request.getSenderId());
+            preparedStatement.setInt(2, request.getReceiverId());
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(RequestsDaoImp.class.getName())
+                    .log(Level.SEVERE, null, ex);
+            return false;
+        }
     }
 
 }
