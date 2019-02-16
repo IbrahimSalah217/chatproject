1c953f5da4089a7debf1295741c6a107eaef2878
Ibrahim Yousre <ib.yousre@gmail.com>
2019-02-16 09:34:22 +0200

    integrate register

--- Client/src/main/java/com/jets/chatproject/client/views/login/FirstScreenController.java
+++ Client/src/main/java/com/jets/chatproject/client/views/login/FirstScreenController.java
@@ -4,13 +4,21 @@
  * and open the template in the editor.
  */
 package com.jets.chatproject.client.views.login;
-import  com.jets.chatproject.client.ChatApp;
+
+import com.jets.chatproject.client.ChatApp;
+import com.jets.chatproject.client.views.register.RegisterController;
+import java.io.IOException;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -32,7 +40,7 @@ public class FirstScreenController implements Initializable {
     private Button login;
     @FXML
     private ImageView logoViewer;
-    
+
     Stage stage;
 
     public FirstScreenController(Stage stage) {
@@ -42,25 +50,35 @@ public class FirstScreenController implements Initializable {
 //    public FirstScreenController() {
 //        System.out.println("dummychat.FirstScreenController.<init>()2222222222222");
 //    }
-
-    
     /**
      * Initializes the controller class.
-    
+     *
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("dummychat.FirstScreenController.initialize()");
         // TODO
-    }    
+    }
 
     @FXML
     private void regiserInformation(MouseEvent event) {
-        
+
     }
 
     @FXML
     private void registerAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            RegisterController controller = new RegisterController(stage);
+            fxmlLoader.setController(controller);
+            Parent root = fxmlLoader.load(RegisterController.class.getResource("register.fxml").openStream());
+            Scene scene = new Scene(root);
+            
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(FirstScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
@@ -75,5 +93,5 @@ public class FirstScreenController implements Initializable {
     @FXML
     private void logoInformation(MouseEvent event) {
     }
-    
+
 }
