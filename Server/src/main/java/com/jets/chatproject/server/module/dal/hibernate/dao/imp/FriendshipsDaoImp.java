7cf72dca7a0f804c8b5d1199af6a858f6207f855
Ibrahim <Ibrahim@Ibrahim-PC>
2019-03-12 18:58:42 +0200

    add friendShipsDaoImp

--- Server/src/main/java/com/jets/chatproject/server/module/dal/hibernate/dao/imp/FriendshipsDaoImp.java
+++ Server/src/main/java/com/jets/chatproject/server/module/dal/hibernate/dao/imp/FriendshipsDaoImp.java
@@ -7,8 +7,15 @@ package com.jets.chatproject.server.module.dal.hibernate.dao.imp;
 
 import com.jets.chatproject.server.module.dal.dao.FriendshipsDao;
 import com.jets.chatproject.server.module.dal.entities.Friendship;
+import com.jets.chatproject.server.module.dal.hibernate.entity.DirectMessages;
+import com.jets.chatproject.server.module.dal.hibernate.entity.Friendships;
+import com.jets.chatproject.server.module.dal.hibernate.entity.FriendshipsId;
+import com.jets.chatproject.server.module.dal.hibernate.entity.Users;
+import java.util.LinkedList;
 import java.util.List;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -22,6 +29,11 @@ public class FriendshipsDaoImp implements FriendshipsDao {
         this.session = session;
     }
 
+    private Friendship mapFriendShip(Friendships dbFriendships) {
+        return new Friendship(dbFriendships.getUsersByUserId().getUserId(), dbFriendships.getUsersByFriendId().getUserId(),
+                dbFriendships.getCategory(), dbFriendships.getBlocked(), dbFriendships.getDirectMessages().getId());
+    }
+
     @Override
     public void addMitualFriendship(int userId, int anotherUserId) throws Exception {
         insert(new Friendship(userId, anotherUserId, "", false, -1));
@@ -30,27 +42,83 @@ public class FriendshipsDaoImp implements FriendshipsDao {
 
     @Override
     public Friendship findByUserAndFriend(int userId, int friendId) throws Exception {
-        return null;
+        Friendships friendships;
+        Friendship friendship;
+        Criteria criteria = session.createCriteria(Friendships.class);
+        friendships = (Friendships) criteria.add(Restrictions.idEq(new FriendshipsId(userId, friendId))).uniqueResult();
+        friendship = mapFriendShip(friendships);
+        return friendship;
     }
 
     @Override
     public List<Friendship> getAllFriendshipsForUser(int userId) throws Exception {
-        return null;
+        List<Friendships> friendshipsList;
+        Criteria criteria = session.createCriteria(Friendships.class);
+        friendshipsList = criteria.createCriteria("usersByUserId").add(Restrictions.idEq(userId)).list();
+        List<Friendship> friendshipList = new LinkedList<>();
+        friendshipsList.forEach((friendships) -> {
+            friendshipList.add(mapFriendShip(friendships));
+        });
+        return friendshipList;
     }
 
     @Override
     public int insert(Friendship object) throws Exception {
-        return 0;
+        try {
+            Friendships friendships = createFriendships(object);
+            session.beginTransaction();
+            session.persist(friendships);
+            session.getTransaction().commit();
+            return friendships.getDirectMessages().getId();
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
     public boolean update(Friendship object) throws Exception {
-        return true;
+        try {
+            Friendships friendships = createFriendships(object);
+            session.beginTransaction();
+            session.saveOrUpdate(friendships);
+            session.getTransaction().commit();
+            return true;
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
     public boolean delete(Friendship object) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            Friendships friendships = createFriendships(object);
+            session.beginTransaction();
+            session.delete(friendships);
+            session.getTransaction().commit();
+            return true;
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+            throw ex;
+        }
+    }
+
+    private Friendships createFriendships(Friendship friendship) {
+        Criteria criteria = session.createCriteria(DirectMessages.class);
+        DirectMessages directMessages = (DirectMessages) criteria.
+                add(Restrictions.idEq(friendship.getLastSeenMessageId())).uniqueResult();
+        Criteria criteria2 = session.createCriteria(Users.class);
+        Users users = (Users) criteria2.
+                add(Restrictions.idEq(friendship.getUserId())).uniqueResult();
+        Users friends = (Users) criteria2.
+                add(Restrictions.idEq(friendship.getFriendId())).uniqueResult();
+
+        FriendshipsId friendshipsId = new FriendshipsId(friendship.getUserId(), friendship.getFriendId());
+        Friendships friendships = new Friendships(friendshipsId, directMessages, users,
+                friends, friendship.getCategory(), friendship.isBlocked());
+        return friendships;
+
     }
 
 }
